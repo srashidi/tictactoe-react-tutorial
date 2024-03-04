9ef3ead546939d5387b3438c43592f7095a4c74f
Shaunty Rashidi <srashidi@users.noreply.github.com>
2024-03-04 14:26:37 -0800

    Change to text for current move instead of button

--- src/App.js
+++ src/App.js
@@ -70,16 +70,24 @@ export default function Game() {
 
     const moves = history.map((squares, move) => {
         let description;
-        if (move > 0) {
+        if (move === currentMove) {
+            description = 'You are at move #' + move;
+        } else if (move > 0) {
             description = 'Go to move #' + move;
         } else {
             description = 'Go to game start';
         }
-        return (
-            <li key={move}>
-                <button onClick={() => jumpTo(move)}>{description}</button>
-            </li>
-        );
+        if (move === currentMove) {
+            return (
+                <li key={move}>{description}</li>
+            );
+        } else {
+            return (
+                <li key={move}>
+                    <button onClick={() => jumpTo(move)}>{description}</button>
+                </li>
+            );
+        }
     });
 
     return (
